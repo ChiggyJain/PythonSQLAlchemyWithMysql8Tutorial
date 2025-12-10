dc6880dd72b646e5733fecf1c9a36326bc1f1b97
Chirag Jain <cjain9975@gmail.com>
2025-12-10 08:59:43 +0530

    Python SQLAlchemy with Mysql Tutorial

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,12 @@
 
 from app.database.db import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
 
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
+    name = Column(String(100), nullable=False)
+    email = Column(String(200), unique=True, nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
     
